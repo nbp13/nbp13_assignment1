1e42ac22c6e6c374f78467ef798bb2e5b9eb95fa
nbp13 <160253179+nbp13@users.noreply.github.com>
2025-09-30 20:52:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("venmo").addEventListener("click", function() {
+    window.open("https://venmo.com/Nessa-Purohit");
+  });
+  
+  
+  document.getElementById("booking-btn").addEventListener("click", function() {
+    alert("Thanks for booking! I’ll get back to you soon 💖");
+  });  
